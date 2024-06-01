f3145a8074f51a9bdebc641b9953ce5554448aaf
Cure232 <timtim2004232@gmail.com>
2024-06-01 14:34:29 +0400

    base functions written

--- LinkedList.h
+++ LinkedList.h
@@ -5,206 +5,132 @@
 
 using namespace std;
 
-template<typename T>
-class LinkedList {
+class SearchTree {
 protected:
-	struct Node {
-		Node* next;
-		Node* prev;
-		T data;
+	struct TreeNode {
+		TreeNode* left;
+		TreeNode* right;
+		int key;
 
-		Node(const T& _data, Node* _prev = nullptr, Node* _next = nullptr) {
-			data = _data;
-			prev = _prev;
-			next = _next;
+		TreeNode(const int& _key, TreeNode* _left = nullptr, TreeNode* _right = nullptr) {
+			key = _key;
+			left = _left;
+			right = _right;
 		}
 	};
 
 private:
 	int length = 0;
-	Node* head_ptr = nullptr;
-	Node* tail_ptr = nullptr;
+	TreeNode* root = nullptr;
+	
+	void print_all(const TreeNode* tn) const{
+		if (tn != nullptr) {
+			print_all(tn->left);
+			cout << tn->key << " ";
+			print_all(tn->right);
+		}
+		else return;
+	}
 
 public:
+	SearchTree() = default;
+
+	SearchTree(const SearchTree& st) {
+		length = st.length;
+		//push_tail(st);
+	}
+
+	SearchTree& operator=(const SearchTree& rhs) {
+		if (this == &rhs) {
+			return *this;
+		}
+
+		this->~SearchTree();
+
+		//this->push_tail(rhs);
+		return *this;
+	}
+
+	~SearchTree() {
+		if (length > 0) {
+			TreeNode* iter_ptr = this->root;
+			TreeNode* next_node;
+		}
+		root = nullptr;
+		length = 0;
+	}
 
 	int get_length() const {
 		return length;
 	}
 
-	LinkedList() {
-
+	void print_all() const {
+		print_all(this->root);
 	}
 
-	LinkedList(const LinkedList& ll) {
-		length = ll.length;
-		push_tail(ll);
-	}
-
-	LinkedList& operator=(const LinkedList& rhs) {
-		if (this == &rhs) {
-			return *this;
+	bool contains(const int& key) const{
+		if (root == nullptr) {
+			return false;
 		}
 
-		~this();
-		this->push_tail(rhs);
-
-		return *this;
-	}
-
-	~LinkedList() {
-		if (length > 0) {
-			Node* iter_ptr = this->head_ptr;
-			Node* next_node;
-			for (next_node = iter_ptr->next; iter_ptr != this->tail_ptr; next_node = iter_ptr->next) {
-				delete(iter_ptr);
-				iter_ptr = next_node;
+		TreeNode* iter = root;
+		while (true) {
+			if (key < iter->key) {
+				iter = iter->left;
+				continue;
 			}
-			delete(iter_ptr);
-		}
-		tail_ptr = nullptr;
-		head_ptr = nullptr;
-		length = 0;
-	}
-
-	void push_tail(const T& item) {
-		this->length += 1;
-		Node* node = new Node(item);
-
-		if (tail_ptr != nullptr) {
-			tail_ptr->next = node;
-			node->prev = tail_ptr;
-			tail_ptr = node;
-		}
-		else {
-			tail_ptr = node;
-			head_ptr = node;
-		}
-	}
-
-	void push_tail(const LinkedList& list) {
-		if (list.length > 0) {
-			Node* iter_ptr;
-			for (iter_ptr = list.head_ptr; iter_ptr != list.tail_ptr; iter_ptr = iter_ptr->next) {
-				this->push_tail(iter_ptr->data);
+			else if (key > iter->key) {
+				iter = iter->right;
+				continue;
 			}
-			this->push_tail(iter_ptr->data);
-		}
-	}
-
-	void push_head(const T& item) {
-		this->length += 1;
-		Node* node = new Node(item);
-
-		if (head_ptr != nullptr) {
-			head_ptr->prev = node;
-			node->next = head_ptr;
-			head_ptr = node;
-		}
-		else {
-			tail_ptr = node;
-			head_ptr = node;
-		}
-	}
-
-	void push_head(const LinkedList& list) {
-		if (list.length > 0) {
-			Node* iter_ptr;
-			for (iter_ptr = list.head_ptr; iter_ptr != list.tail_ptr; iter_ptr = iter_ptr->next) {
-				this->push_head(iter_ptr->data);
+			else if (key == iter->key) {
+				return true;
 			}
-			this->push_head(iter_ptr->data);
+			else return false;
 		}
 	}
 
-	T pop_head() {
-		if (head_ptr != nullptr) {
-			T content = head_ptr->data;
-			
-			if (length > 1) {
-				head_ptr = head_ptr->next;
-				delete(head_ptr->prev);
-				head_ptr->prev = nullptr;
+	bool insert(const int& key) {
+		if (root == nullptr) {
+			root = new TreeNode(key);
+			return true;
+		}
+
+		if (this->contains(key)) {
+			return false;
+		}
+
+		TreeNode* iter = root;
+		while (true)
+		{
+			if (key < iter->key){
+				if (iter->left == nullptr)
+				{
+					TreeNode* new_tree_node = new TreeNode(key);
+					iter->left = new_tree_node;
+				}
+				else {
+					iter = iter->left;
+				}
 			}
 			else {
-				delete(this);
-				head_ptr = nullptr;
-				tail_ptr = nullptr;
-			}
-			this->length -= 1;
-			return content;
-		}
-		else throw runtime_error("slkdgjos;ag");
-	}
-
-	T pop_tail() {
-		if (tail_ptr != nullptr) {
-			T content = tail_ptr->data;
-
-			if (length > 1) {
-				tail_ptr = tail_ptr->prev;
-				delete(tail_ptr->next);
-				tail_ptr->next = nullptr;
-			}
-			else {
-				delete(tail_ptr);
-				head_ptr = nullptr;
-				tail_ptr = nullptr;
-			}
-			this->length -= 1;
-			return content;
-		}
-		else throw runtime_error("dfsgfd");
-	}
-
-	void delete_node(const T& to_delete) {
-		Node* iter_ptr = head_ptr;
-		Node* next_node;
-		Node* prev_node;
-		while (iter_ptr != nullptr && length > 0) {
-			next_node = iter_ptr->next;
-			prev_node = iter_ptr->prev;
-			if (iter_ptr->data == to_delete) {
-
-				if (next_node != nullptr) {
-					next_node->prev = iter_ptr->prev;
+				if (key > iter->key) {
+					if (iter->right == nullptr)
+					{
+						TreeNode* new_tree_node = new TreeNode(key);
+						iter->right = new_tree_node;
+					}
+					else {
+						iter = iter->right;
+					}
 				}
-				if (prev_node != nullptr) {
-					prev_node->next = iter_ptr->next;
-				}
-
-				if (iter_ptr == this->head_ptr) {
-					head_ptr = next_node;
-				}
-				if (iter_ptr == this->tail_ptr) {
-					head_ptr = prev_node;
-				}
-
-				delete(iter_ptr);
-				length -= 1;
 			}
-			iter_ptr = next_node;
 		}
 	}
 
-	T operator[](const int& i) const {
-		if (i >= 0 && i < length) {
-			Node* iter_ptr = this->head_ptr;
-			for (int j = 0; j < i; j++) {
-				iter_ptr = iter_ptr->next;
-			}
-			return iter_ptr->data;
-		}
-		else throw out_of_range("sd");
-	}
 
-	T& operator[](const int& i) {
-		if (i >= 0 && i < length) {
-			Node* iter_ptr = this->head_ptr;
-			for (int j = 0; j < i; j++) {
-				iter_ptr = iter_ptr->next;
-			}
-			return (iter_ptr->data);
-		}
-		else throw runtime_error("bs");
+
+	bool erase(const int& key) {
+		
 	}
 };
